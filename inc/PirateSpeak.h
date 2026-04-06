6280416add286758dc9e08e0aa0f3152bb4f9d33
spencerwhite2-hash <spencerwhite2@mail.weber.edu>
2026-04-05 20:44:12 -0600

    PIRATE

--- inc/PirateSpeak.h
+++ inc/PirateSpeak.h
@@ -9,26 +9,34 @@
 using namespace std;
 
 class Pirate {
-    public:
-        string translateWord(string word){
-            //TODO: Enter the path to the text file in the double quotes
-            ifstream fin("", ios::in);
-            string english, pirate;
+public:
+    string translateWord(string word) {
+        ifstream fin("C:/Users/subaw/OneDrive/Desktop/HW12/files/pirate.csv", ios::in);
+        string english, pirate;
 
-            //TODO: The way that an ifstream works is that it reads everything in the 
-            //      csv file. 
-            //      1) Create a while loop that does not terminate until the file
-            //         has been read fully
-            //      2) using getline(), read the english word
-            //      3) using getline(), read the pirate word
-            //      4) if the english word equals the word that is being passed,
-            //         then close the file and return the pirate word
-            //      5) otherwise, close the file and just return the word being passed
+        while (getline(fin, english, ',') && getline(fin, pirate)) {
+            if (english == word) {
+                fin.close();
+                return pirate;
+            }
         }
 
-        void addToFile(string english, string pirate){
-            //TODO: append the passed words to the csv file and close the file
-            //      remember to delimit the words with a comma
+        fin.close();
+        return word;
+    }
+
+    void addToFile(string english, string pirate) {
+        ofstream fout("C:/Users/subaw/OneDrive/Desktop/HW12/files/pirate.csv", ios::app);
+
+        if (!fout) {
+            cout << "File did not open for writing." << endl;
+            return;
         }
+
+        fout << english << "," << pirate << endl;
+        fout.close();
+
+        cout << "Word added successfully!" << endl;
+    }
 };
 #endif
--- src/PirateSpeak.cpp
+++ src/PirateSpeak.cpp
@@ -10,6 +10,15 @@ enum Choices {TRANSLATE = 1, ADD, QUIT};
 //      and the return type is int.
 //      In the function prompt the user to choose 1 of three menus. Translate, Add to
 //      Dictionary, or Quit.
+int menu(){
+  int choice;
+  cout << "1) Translate" << endl;
+  cout << "2) Add to Dictionary" << endl;
+  cout << "3) Quit" << endl;
+  cout << "Enter your choice: ";
+  cin >> choice;
+  return choice;
+}
 
 //TODO: Create the addWord() function. It takes no parameters and the return type
 //      is void.
@@ -17,11 +26,29 @@ enum Choices {TRANSLATE = 1, ADD, QUIT};
 //      2) prompt the user to enter a word in english
 //      3) prompt the user to enter the pirate translation
 //      4) call the object function called addToFile and pass both words as parameters
+void addWord(){
+  Pirate p;
+  string english, pirate;
+  cout << "Enter the English word: ";
+  cin >> english;
+  cout << "Enter the Pirate translation: ";
+  cin >> pirate;
+  p.addToFile(english, pirate);
+}
 
 //TODO: Create the translate() function. It takes no parameters and the return type is void
 //      1) create a Pirate object
 //      2) prompt the user to enter a word
 //      3) call the object function called translateWord and pass the word as a parameter
+
+void translate(){
+  Pirate p;
+  string word;
+  cout << "Enter the word to translate: ";
+  cin >> word;
+  string translation = p.translateWord(word);
+  cout << "Pirate translation: " << translation << endl;
+}
 
 int main() {
   int choice;
